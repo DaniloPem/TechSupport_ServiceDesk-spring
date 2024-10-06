36ca15c0b554aaa6240c97ca48fb0c93c8196d10
Danilo Pezo <dpezomendoza@gmail.com>
2024-10-06 17:26:53 -0300

    Implementado método para retornar lista vazía

--- src/main/java/projetotechsupport/apitechsupport/service/UsuarioService.java
+++ src/main/java/projetotechsupport/apitechsupport/service/UsuarioService.java
@@ -7,6 +7,7 @@ import projetotechsupport.apitechsupport.model.usuario.Usuario;
 import projetotechsupport.apitechsupport.model.usuario.UsuarioRecord;
 import projetotechsupport.apitechsupport.model.usuario.UsuarioRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,6 +17,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
     public List<UsuarioRecord> findByCodigoLike(String codigo) {
+        if (codigo.isEmpty()) return Collections.emptyList();
         List<Usuario> usuarios = usuarioRepository.findByCodigoLike('%' + codigo + '%');
         return usuarios.stream().map(UsuarioRecord::new).toList();
     }
