936e2abc15785460ff5d668b0dbdc09ea081bacd
Danilo Pezo <dpezomendoza@gmail.com>
2024-12-31 18:16:34 -0300

    Query implementada para buscar usuarios por filtro

--- src/main/java/projetotechsupport/apitechsupport/model/usuario/UsuarioRepository.java
+++ src/main/java/projetotechsupport/apitechsupport/model/usuario/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package projetotechsupport.apitechsupport.model.usuario;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,6 +11,16 @@ import java.util.List;
 @Repository
 public interface
 UsuarioRepository extends JpaRepository<Usuario, Long> {
-
     List<Usuario> findByCodigoLike(String codigo);
+
+    @Query("""
+            SELECT usuario FROM Usuario usuario
+            LEFT JOIN usuario.gruposAssignados gruposAssignados
+            WHERE usuario.codigo LIKE :filtro OR
+            usuario.nome LIKE :filtro OR
+            usuario.email LIKE :filtro OR
+            COALESCE(usuario.telefone, '') LIKE:filtro OR
+            COALESCE(gruposAssignados.nome, '') LIKE:filtro
+            """)
+    Page<Usuario> findByFiltro(String filtro, Pageable pageable);
 }
