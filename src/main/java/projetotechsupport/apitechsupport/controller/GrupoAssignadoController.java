e811f1f2ff7e468f7241bb2d42365fe5bfd37fb9
Danilo Pezo <dpezomendoza@gmail.com>
2025-01-01 12:24:52 -0300

    Implementado getmapping para listar todos os grupos assignados com paginação

--- src/main/java/projetotechsupport/apitechsupport/controller/GrupoAssignadoController.java
+++ src/main/java/projetotechsupport/apitechsupport/controller/GrupoAssignadoController.java
@@ -1,7 +1,11 @@
 package projetotechsupport.apitechsupport.controller;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import projetotechsupport.apitechsupport.model.grupoAssignado.GrupoAssignadoPageDTO;
 import projetotechsupport.apitechsupport.service.GrupoAssignadoService;
 import projetotechsupport.apitechsupport.shared.dtos.IdNomeDTO;
 
@@ -13,6 +17,13 @@ import java.util.List;
 public class GrupoAssignadoController {
 
     private final GrupoAssignadoService grupoAssignadoService;
+
+    @GetMapping
+    public @ResponseBody GrupoAssignadoPageDTO listarAllGruposAssignados(@RequestParam String filter,
+                                                                         @RequestParam(defaultValue = "0") @PositiveOrZero int page,
+                                                                         @RequestParam(defaultValue = "30") @Positive @Max(30) int pageSize) {
+        return grupoAssignadoService.findAllGruposAssignados(filter, page, pageSize);
+    }
 
     @GetMapping("/por-categoria/{categoriaId}")
     public @ResponseBody List<IdNomeDTO> listar(@PathVariable Long categoriaId) {
