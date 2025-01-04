0f1457c4a2ef45752fdfb047ccc519215f0b6bb8
Danilo Pezo <dpezomendoza@gmail.com>
2025-01-04 15:56:16 -0300

    Implementado método da requisição getmapping para listar as tags

--- src/main/java/projetotechsupport/apitechsupport/controller/TagController.java
+++ src/main/java/projetotechsupport/apitechsupport/controller/TagController.java
@@ -1,8 +1,12 @@
 package projetotechsupport.apitechsupport.controller;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import projetotechsupport.apitechsupport.model.tag.Tag;
+import projetotechsupport.apitechsupport.model.tag.TagPageDTO;
 import projetotechsupport.apitechsupport.model.tag.TagRepository;
 import projetotechsupport.apitechsupport.service.TagService;
 import projetotechsupport.apitechsupport.shared.dtos.IdNomeDTO;
@@ -17,6 +21,13 @@ import java.util.stream.Collectors;
 public class TagController {
 
     private final TagService tagService;
+
+    @GetMapping
+    public @ResponseBody TagPageDTO listarAllTags(@RequestParam String filter,
+                                                  @RequestParam(defaultValue = "0") @PositiveOrZero int page,
+                                                  @RequestParam(defaultValue = "30") @Positive @Max(30) int pageSize){
+        return tagService.findAllTags(filter, page, pageSize);
+    }
 
     @GetMapping("/por-categoria/{categoriaId}")
     public @ResponseBody List<IdNomeDTO> listar(@PathVariable Long categoriaId) {
