88562979bf8a16b5253b0cb8c29050514faf4905
Danilo Pezo <dpezomendoza@gmail.com>
2025-01-04 15:27:03 -0300

    Implementado query para encontrar a lista de tags segundo o filtro

--- src/main/java/projetotechsupport/apitechsupport/model/tag/TagRepository.java
+++ src/main/java/projetotechsupport/apitechsupport/model/tag/TagRepository.java
@@ -1,6 +1,10 @@
 package projetotechsupport.apitechsupport.model.tag;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import projetotechsupport.apitechsupport.model.categoria.Categoria;
 import projetotechsupport.apitechsupport.shared.dtos.IdNomeDTO;
 
 import java.util.List;
@@ -8,5 +12,15 @@ import java.util.List;
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
     List<Tag> findByCategoriaId(Long categoriaId);
+
+    @Query("""
+            SELECT tag FROM Tag tag
+            JOIN tag.categoria categoria
+            LEFT JOIN tag.subtags subtags
+            WHERE tag.nome LIKE :filtro OR
+            categoria.nome LIKE :filtro OR
+            COALESCE(subtags.nome, '') LIKE :filtro
+            """)
+    Page<Tag> findByFiltro(String filtro, Pageable pageable);
 
 }
